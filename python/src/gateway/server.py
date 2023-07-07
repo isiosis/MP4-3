ab5d98e10458ee7f4531798fe58e09a4e10e5dce
Isiosis <se.minyahel.fekadu@gmail.com>
2023-07-07 17:49:15 +0200

    added rabbit-mq kubernetes manifests

--- python/src/gateway/server.py
+++ python/src/gateway/server.py
@@ -10,8 +10,7 @@ from storage import util
 
 server = Flask(__name__)
 
-server.config["MONGO_URI"] = os.environ.get(
-    "mongodb://host.minikube.internal:27017/videos")
+server.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/videos"
 
 mongo = PyMongo(server)
 
